1b41831846748551a35e6a08387171f475e91e08
Johan Lövgren <lojohan@student.chalmers.se>
2016-10-22 12:24:41 +0200

    Deleted some random saved buffers.

--- entities/entityclass/Entity.cpp
+++ entities/entityclass/Entity.cpp
@@ -1,27 +1,27 @@
 #include "../entityheaders/Entity.h"
 
 Entity::Entity(int x, int y) {
-    this.x = x;
-    this.y = y;
+    this->x = x;
+    this->y = y;
 }
 
 void Entity::setX(int x) {
-    this.x = x;
+    this->x = x;
 }
 
 void Entity::setY(int y){
-    this.y = y;
+    this->y = y;
 }
 
 void Entity::setCoords(int x, int y){
-    this.x = x;
-    this.y = y;
+    this->x = x;
+    this->y = y;
 }
 
 int Entity::getX(){
-    return this.x;
+    return this->x;
 }
 
 int Entity::getY(){
-    return this.y;
+    return this->y;
 }
