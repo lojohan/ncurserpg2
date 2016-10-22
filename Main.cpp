57c9e9790836f68792044030f4c38e881b009d6e
Johan Lövgren <lojohan@student.chalmers.se>
2016-10-22 12:41:25 +0200

    Tested Entity class.

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "entities/entityheaders/Entity.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!";
+    int x = 20;
+    int y = 0;
+    Entity * entity = new Entity(x,y);
+    
+    cout << "X: " << entity->getX() << ", Y: " << entity->getY();
 }
