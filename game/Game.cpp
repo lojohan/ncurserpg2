c3651d48dbd22299da0a48260022955cec5cd311
Johan Lövgren <lojohan@student.chalmers.se>
2016-10-23 13:14:18 +0200

    Drawing properly cleared between refreshes.

--- game/Game.cpp
+++ game/Game.cpp
@@ -2,7 +2,50 @@
 
 Game::Game() {
     
+    this->init();
     
+    // placeholder
+    MovableEntity mve = *(new MovableEntity(10,10,"p"));
+    mvEntityList.push_back(mve);
+    
+    while(true) {
+    
+        this->draw();
+        // placeholder
+        int ch = playerInput(this->menu_win);
+        updateMovables(ch);
+        
+    }
+    
+    this->end();
+}
+
+void Game::fillTileMap() {
+    (this->map).parseMap();    
+    for(std::vector<Tile>::iterator it = (this->map).getTileMap().begin(); it != (this->map).getTileMap().end(); ++it) {
+        (this->tileList).push_back( *(it) );
+    }
+}
+
+void Game::updateMovables(int ch) {
+        for(std::vector<MovableEntity>::iterator it = mvEntityList.begin(); it != mvEntityList.end(); ++it) {
+        it->move(ch);
+    }
+}
+
+void Game::drawTiles() {
+    for(std::vector<Tile>::iterator it = tileList.begin(); it != tileList.end(); ++it) {
+        it->draw();
+    }
+}
+
+void Game::drawMovables() {
+    for(std::vector<MovableEntity>::iterator it = mvEntityList.begin(); it != mvEntityList.end(); ++it) {
+        it->draw();
+    }
+}
+
+void Game::init() {
     initscr();
     
     menu_win = newwin(30,30,10,10);
@@ -13,28 +56,24 @@ Game::Game() {
 	cbreak();	/* Line buffering disabled. pass on everything */
     curs_set(0);
     fillTileMap();
-    
-    MovableEntity mve = *(new MovableEntity(10,10,"p"));
-    
-    while(true) {
-        drawTiles();
-        int ch = playerInput(this->menu_win);
-        mve.move(ch);
-        mve.draw();
-        refresh();
-    }
+}
+
+void Game::end() {
     endwin();
 }
 
-void Game::fillTileMap() {
-    (this->map).parseMap();    
-    for(std::vector<Tile>::iterator it = (this->map).getTileMap().begin(); it != (this->map).getTileMap().end(); ++it) {
-        (this->tileList).push_back( *(it) );
+void Game::clearBeforeDraw() {
+    for(int i = 0; i < MENU_HEIGHT; i++) {
+        for(int j = 0; j < MENU_WIDTH; j++) {
+            mvprintw(i,j," ");
+        }
     }
 }
 
-void Game::drawTiles() {
-    for(std::vector<Tile>::iterator it = tileList.begin(); it != tileList.end(); ++it) {
-        it->draw();
-    }
+// separate drawing into spearate class 
+void Game::draw() {
+    clearBeforeDraw();
+    drawTiles();
+    drawMovables();
+    refresh();
 }
--- game/Game.h
+++ game/Game.h
@@ -10,13 +10,28 @@
 class Game {
     private: 
         std::vector<Tile> tileList;
+        std::vector<MovableEntity> mvEntityList;
         Map map;
+        
+        // should be in drawing bit
         WINDOW *menu_win;
+        const static int MENU_HEIGHT = 30;
+        const static int MENU_WIDTH = 30;
+        const static int START_X = 0;
+        const static int START_Y = 0;
 
     public:
         Game();
         void fillTileMap();
+        void init();
+        void end();
+        void updateMovables(int ch);
+        
+        // separate this drawing bit here
+        void clearBeforeDraw();
         void drawTiles();
+        void drawMovables();
+        void draw();
              
 };
 
